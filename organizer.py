5c09d9562ab9851372137a87a2784162c695b6c0
luboskollar <lubos.kollar444@gmail.com>
2026-06-14 22:17:00 +0200

    Reorganize the GUI

--- organizer.py
+++ organizer.py
@@ -49,7 +49,14 @@ def process_files(sorted_list, new_name, extension=None, copy=False):
 def get_folder():
     global source_folder
     source_folder = filedialog.askdirectory()
-    print(source_folder)
+
+    parts = source_folder.split("/")
+    if len(parts) > 3:
+        display_path = "..." + "/".join(parts[-3:])
+    else:
+        display_path = source_folder
+
+    label_path.config(text=display_path)
 
 def run():
     files = get_files()
@@ -71,31 +78,38 @@ window = tk.Tk()
 window.title("Organizer")
 
 # ===================== GUI WIDGETS =====================
-button_choose = tk.Button(window, text="Choose folder", command=get_folder)
-button_choose.pack()
+label_path = tk.Label(window, text="No folder selected", width=10, anchor="w")
+label_path.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky="we")
 
-button_run = tk.Button(window, text="Run", command=run)
-button_run.pack()
+
+button_choose = tk.Button(window, text="Choose folder", command=get_folder, width=25)
+button_choose.grid(row=0, column=1, padx=10, pady=5)
+
+button_run = tk.Button(window, text="Run", command=run, width=20)
+button_run.grid(row=5, column=2, sticky="e", padx=5, pady=10)
 
 label_name = tk.Label(window, text="Entry name")
-label_name.pack()
+label_name.grid(row=1, column=0, sticky="w", padx=5)
 
 entry_name = tk.Entry(window, width=30)
-entry_name.pack()
+entry_name.grid(row=1, column=1, padx=5)
+
+label_sort = tk.Label(window, text="Sort by:")
+label_sort.grid(row=0, column=2, sticky="w", padx=5)
 
 sort_choice = tk.StringVar(value="created")
 radio_created = tk.Radiobutton(window, text="Created", variable=sort_choice, value="created")
-radio_created.pack()
+radio_created.grid(row=1, column=2, sticky="w", padx=5)
 
 radio_name = tk.Radiobutton(window, text="Name", variable=sort_choice, value="name")
-radio_name.pack()
+radio_name.grid(row=2, column=2, sticky="w", padx=5)
 
 copy_var = tk.BooleanVar(value=False)
 check_copy = tk.Checkbutton(window, text="Copy (keep originals)", variable=copy_var)
-check_copy.pack()
+check_copy.grid(row=4, column=0, sticky="w", padx=5)
 
 convert_var = tk.BooleanVar(value=False)
 check_convert = tk.Checkbutton(window, text="Convert PNG to JPG (this copies files by default)", variable=convert_var)
-check_convert.pack()
+check_convert.grid(row=4, column=1, sticky="w", padx=5)
 
 window.mainloop()

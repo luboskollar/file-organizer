8caee3578ea056d17a70e9de3a0ed8ace89cd9f7
luboskollar <lubos.kollar444@gmail.com>
2026-06-14 15:59:35 +0200

    Add file collection from source folder

--- organizer.py
+++ organizer.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+import os
+
+source_folder = "C:/Users/kakad/Desktop/test"
+files = []
+
+for file in os.listdir(source_folder):
+    path = os.path.join(source_folder, file)
+    if os.path.isfile(path):
+        files.append(path)
+print(files)
+print(len(files))
